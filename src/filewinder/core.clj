76eac64278c404a5e3c4eb503602299e0ce1f462
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-02-18 19:55:17 +0800

    Added some code that will filter files from target-dir that match the name of the global source-file.

--- src/filewinder/core.clj
+++ src/filewinder/core.clj
@@ -1,1 +1,6 @@
 (ns filewinder.core)
+(def source-file (java.io.File. "/home/alex/dev/play/clojure/filewinder/src/core.clj"))
+(def target-dir (java.io.File. "/home/alex/dev/play/clojure/filewinder"))
+(defn file-name-match? [file]
+  (= (.getName file) (.getName source-file)))
+(filter file-name-match? (file-seq target-dir))
