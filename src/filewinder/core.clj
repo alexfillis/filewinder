dc49eacf3303ac28854d5c67069d9ddc4cf0adfc
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-02-19 20:39:08 +0800

    Replaced reference to global in file-name-match? with extra parameter. Modified call to filter to use partial to take source-file as the first param.

--- src/filewinder/core.clj
+++ src/filewinder/core.clj
@@ -1,6 +1,6 @@
 (ns filewinder.core)
 (def source-file (java.io.File. "/home/alex/dev/play/clojure/filewinder/src/core.clj"))
 (def target-dir (java.io.File. "/home/alex/dev/play/clojure/filewinder"))
-(defn file-name-match? [file]
-  (= (.getName file) (.getName source-file)))
-(filter file-name-match? (file-seq target-dir))
+(defn file-name-match? [file1 file2]
+  (= (.getName file1) (.getName file2)))
+(filter (partial file-name-match? source-file) (file-seq target-dir))
