059ba962e841f5f0c633b9d7fdcfed13d4bcf6c0
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-02-22 23:58:00 +0800

    Holding the result of the filter and then zipping that up with the source file-seq to match file search results with the file.

--- src/filewinder/core.clj
+++ src/filewinder/core.clj
@@ -1,10 +1,11 @@
 (ns filewinder.core)
-;(def source-file (java.io.File. "/home/alex/dev/play/clojure/filewinder/src/core.clj"))
+
 (def source-dir (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/source"))
-;(def target-dir (java.io.File. "/home/alex/dev/play/clojure/filewinder"))
 (def target-dir (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/target"))
+
 (defn file-name-match? [file1 file2]
   (= (.getName file1) (.getName file2)))
 
-(for [source-file (file-seq source-dir)]
-  (filter (partial file-name-match? source-file) (file-seq target-dir)))
+(def matches (for [source-file (file-seq source-dir)]
+               (filter (partial file-name-match? source-file) (file-seq target-dir))))
+(zipmap (file-seq source-dir) matches)
