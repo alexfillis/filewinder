7b79eb52eb532b6ebd41305f9847dc1b3d0ea350
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-02-25 09:35:01 +0800

    changed matches def into a function so that it can be easily reused and tested

--- src/filewinder/core.clj
+++ src/filewinder/core.clj
@@ -6,6 +6,8 @@
 (defn file-name-match? [file1 file2]
   (= (.getName file1) (.getName file2)))
 
-(def matches (for [source-file (file-seq source-dir)]
-               (filter (partial file-name-match? source-file) (file-seq target-dir))))
+(defn match-files [dir1 dir2]
+  (for [file (file-seq dir1)]
+    (filter (partial file-name-match? file) (file-seq dir2))))
+
 (zipmap (file-seq source-dir) matches)
