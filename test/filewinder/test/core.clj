30f7d46c083b25ba282a4086d353be304ace2798
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-02-25 09:20:03 +0800

    added another test for file-name-match that tests that two files with different names do not match

--- test/filewinder/test/core.clj
+++ test/filewinder/test/core.clj
@@ -4,6 +4,10 @@
 
 (def file1 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/source/tmp.json"))
 (def file2 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/target/tmp.json"))
+(def file3 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/target/one/test.txt"))
 
-(deftest should-return-true-for-two-files-with-same-name
+(deftest should-return-true-for-two-files-with-same-names
   (is (file-name-match? file1 file2)))
+
+(deftest should-return-false-for-two-files-with-different-names
+  (is (not (file-name-match? file1 file3))))
