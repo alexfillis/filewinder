ed131fc6e1abd2d58874d54c215f37460bfe2997
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-02-25 09:14:55 +0800

    added test for file-name-match? that tests that two different files with same name match (i.e. return true)

--- test/filewinder/test/core.clj
+++ test/filewinder/test/core.clj
@@ -2,5 +2,8 @@
   (:use [filewinder.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(def file1 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/source/tmp.json"))
+(def file2 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/target/tmp.json"))
+
+(deftest should-return-true-for-two-files-with-same-name
+  (is (file-name-match? file1 file2)))
