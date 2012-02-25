656a6f61f953ec81527c50617c2f1aeddaf055bf
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-02-25 10:34:48 +0800

    added function to output source dir and the corresponding matching files in the target dir. does not quite work yet, so need to fix it up

--- src/filewinder/core.clj
+++ src/filewinder/core.clj
@@ -9,3 +9,7 @@
 
 (defn find-matches [source-dir target-dir]
   (zipmap (file-seq source-dir) (matching-files source-dir target-dir)))
+
+(defn print-matches [matches]
+  (for [[k vs] matches]
+    (doall (println k) (for [v vs] (println (str "- " v))))))
