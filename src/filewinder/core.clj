0855467f81e25484a314d0f97982e0d26eb758c5
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-02-26 12:48:06 +0800

    Copied filter statement (filters files from specified dir that match the name of the specified file) from matching-files function to it's own function. This makes it easier to test this code in it's own unit and therefore matching files when it is changed it call it as it as we don't have to test this part as well. Also allows potential for reuse.

--- src/filewinder/core.clj
+++ src/filewinder/core.clj
@@ -3,6 +3,9 @@
 (defn file-name-match? [file1 file2]
   (= (.getName file1) (.getName file2)))
 
+(defn match-file [file dir]
+  (filter (partial file-name-match? file) (file-seq dir)))
+
 (defn matching-files [dir1 dir2]
   (for [file (file-seq dir1)]
     (filter (partial file-name-match? file) (file-seq dir2))))
--- test/filewinder/test/core.clj
+++ test/filewinder/test/core.clj
@@ -3,6 +3,7 @@
   (:use [clojure.test]))
 
 (def file1 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/source/tmp.json"))
+(def sfile2 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/source/project/build.xml"))
 (def file2 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/target/tmp.json"))
 (def file3 (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/target/one/test.txt"))
 (def source-dir (java.io.File. "/home/alex/dev/play/clojure/filewinder/test-data/source"))
@@ -13,3 +14,9 @@
 
 (deftest should-return-false-for-two-files-with-different-names
   (is (not (file-name-match? file1 file3))))
+
+(deftest file1-should-match-once-in-target-dir
+  (is (= 1 (count (match-file file1 target-dir)))))
+
+(deftest sfile2-should-not-match-any-file-in-target-dir
+  (is (empty? (match-file sfile2 target-dir))))
