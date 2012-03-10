444c955ca71fe38a98b71cac5e1e35fc88aad514
Alex Fillis <alex.fillis@gravastar.co.uk>
2012-03-10 17:08:25 +0800

    added simple profiling to code.

--- src/filewinder/core.clj
+++ src/filewinder/core.clj
@@ -1,30 +1,31 @@
 (ns filewinder.core
   (:use [clojure.java.io :only (file)])
+  (:use [clojure.contrib.profile :only (profile prof)])
   (:gen-class))
 
 (defn file-name-match? [file1 file2]
-  (= (.getName file1) (.getName file2)))
+  (prof :file-name-match (= (.getName file1) (.getName file2))))
 
 (defn match-file [file dir]
-  (filter (partial file-name-match? file) (file-seq dir)))
+  (prof :match-file (filter (partial file-name-match? file) (file-seq dir))))
 
 (defn match-files [dir1 dir2]
-  (for [file (file-seq dir1)]
-    (match-file file dir2)))
+  (prof :match-files (for [file (file-seq dir1)]
+    (match-file file dir2))))
 
 (defn find-matches [source-dir target-dir]
-  (zipmap (file-seq source-dir) (match-files source-dir target-dir)))
+  (prof :find-matches (zipmap (file-seq source-dir) (match-files source-dir target-dir))))
 
 (defn filter-no-match [matches]
-  (select-keys matches (for [[k v] matches :when (not-empty v)] k)))
+  (prof :filter-no-match (select-keys matches (for [[k v] matches :when (not-empty v)] k))))
 
 (defn print-matches [matches]
-  (doseq [[k vs] matches :when (not-empty vs)]
+  (prof :print-matches (doseq [[k vs] matches :when (not-empty vs)]
     (println (str k))
     (doseq [v vs]
-      (println (str "- " v))))
+      (println (str "- " v)))))
   (flush))
 
 (defn -main[source-dir target-dir]
-  (print-matches (find-matches (file source-dir) (file target-dir))))
+  (profile (print-matches (find-matches (file source-dir) (file target-dir)))))
 
