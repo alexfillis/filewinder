893570c2164cad08735453329aa4b87435034b41
Alex Fillis <alexfillis@gmail.com>
2012-08-05 14:00:02 +0800

    Changed functions to take file-seq instead of directory as input. This means that each function does not have to turn the directory into a file-seq before operating on it, removing many redundant calls to file-seq. The functions now simply take a file-seq as input. It is up to the caller to provide a file-seq.

--- src/filewinder/core.clj
+++ src/filewinder/core.clj
@@ -6,21 +6,21 @@
 (defn is-dir? [file]
   (.isDirectory file))
 
-(defn files-in [dir]
-  (remove is-dir? (file-seq dir)))
+(defn files-in [file-seq]
+  (remove is-dir? file-seq))
 
 (defn file-name-match? [file1 file2]
   (prof :file-name-match (= (.getName file1) (.getName file2))))
 
-(defn match-file [file dir]
-  (prof :match-file (filter (partial file-name-match? file) (file-seq dir))))
+(defn match-file-name [file file-seq]
+  (prof :match-file (filter (partial file-name-match? file) file-seq)))
 
-(defn match-files [dir1 dir2]
-  (prof :match-files (for [file (file-seq dir1)]
-    (match-file file dir2))))
+(defn match-file-names [file-seq-1 file-seq-2]
+  (prof :match-files (for [file file-seq-1]
+    (match-file-name file file-seq-2))))
 
-(defn find-matches [source-dir target-dir]
-  (prof :find-matches (zipmap (file-seq source-dir) (match-files source-dir target-dir))))
+(defn find-matching-file-names [source-file-seq target-file-seq]
+  (prof :find-matches (zipmap source-file-seq (match-file-names source-file-seq target-file-seq))))
 
 (defn filter-no-match [matches]
   (prof :filter-no-match (select-keys matches (for [[k v] matches :when (not-empty v)] k))))
@@ -32,6 +32,6 @@
       (println (str "- " v)))))
   (flush))
 
-(defn -main[source-dir target-dir]
-  (profile (print-matches (find-matches (files-in (file source-dir)) (files-in (file target-dir))))))
+(defn -main[source-dir-path target-dir-path]
+  (profile (print-matches (find-matching-file-names (files-in (file-seq (file source-dir-path))) (files-in (file-seq (file target-dir-path)))))))
 
--- test/filewinder/test/core.clj
+++ test/filewinder/test/core.clj
@@ -13,8 +13,8 @@
 (def tfile2 (file "/home/alex/dev/play/clojure/filewinder/test-data/target/one/test.txt"))
 (def tfile3 (file "/home/alex/dev/play/clojure/filewinder/test-data/target/one/tmp.json"))
 (def tfile4 (file "/home/alex/dev/play/clojure/filewinder/test-data/target/one/tmp.xml"))
-(def source-dir (file "/home/alex/dev/play/clojure/filewinder/test-data/source"))
-(def target-dir (file "/home/alex/dev/play/clojure/filewinder/test-data/target"))
+(def source-file-seq (file-seq (file "/home/alex/dev/play/clojure/filewinder/test-data/source")))
+(def target-file-seq (file-seq (file "/home/alex/dev/play/clojure/filewinder/test-data/target")))
 
 (def matches {sfile1 (seq '(tfile1 tfile3)),
                  sfile2 '(),
@@ -28,7 +28,7 @@
   (is (not (is-dir? sfile1))))
 
 (deftest files-in-tests
-  (is (= 4 (count (files-in source-dir)))))
+  (is (= 4 (count (files-in source-file-seq)))))
 
 (deftest should-return-true-for-two-files-with-same-names
   (is (file-name-match? sfile1 tfile1)))
@@ -36,24 +36,24 @@
 (deftest should-return-false-for-two-files-with-different-names
   (is (not (file-name-match? sfile1 tfile2))))
 
-(deftest sfile3-should-match-once-in-target-dir
-  (is (= 1 (count (match-file sfile3 target-dir)))))
+(deftest sfile3-should-match-once-in-target-file-seq
+  (is (= 1 (count (match-file-name sfile3 target-file-seq)))))
 
-(deftest sfile1-should-match-twice-in-target-dir
-  (is (= 2 (count (match-file sfile1 target-dir)))))
+(deftest sfile1-should-match-twice-in-target-file-seq
+  (is (= 2 (count (match-file-name sfile1 target-file-seq)))))
 
-(deftest sfile2-should-not-match-any-file-in-target-dir
-  (is (empty? (match-file sfile2 target-dir))))
+(deftest sfile2-should-not-match-any-file-in-target-file-seq
+  (is (empty? (match-file-name sfile2 target-file-seq))))
 
-(deftest source-dir-should-match-three-files-in-target-dir
-  (let [matches (match-files source-dir target-dir)]
+(deftest source-file-seq-should-match-three-files-in-target-file-seq
+  (let [matches (match-file-names source-file-seq target-file-seq)]
     (is (= 6 (count matches)))
     (is (= 3 (count (remove empty? matches))))))
 
-(deftest should-return-map-with-source-dir-as-key-matches-as-values
-  (let [matches (find-matches source-dir target-dir)]
+(deftest should-return-map-with-source-file-seq-as-key-matches-as-values
+  (let [matches (find-matching-file-names source-file-seq target-file-seq)]
     (is (= 6 (count matches)))
-    (is (empty? (get (find-matches source-dir  target-dir) sfile5)))
+    (is (empty? (get (find-matching-file-names source-file-seq  target-file-seq) sfile5)))
     (is (empty? (get matches sfile6)))
     (is (empty? (get matches sfile2)))
     (is (= 1 (count (get matches sfile3))))
